37bdfefeb5b27d8d453819c23c199d92ae8a1901
AnnaSergeenkova <lutekAnn@yandex.ru>
2021-12-01 21:00:39 +0300

    start

---
+++ src/main/java/pro/sky/springtesthw3/SpringTestHw3Application.java
@@ -0,0 +1,13 @@
+package pro.sky.springtesthw3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTestHw3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTestHw3Application.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/springtesthw3/SpringTestHw3ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.springtesthw3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTestHw3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
